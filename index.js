41593886275e0359ac64e7f6719e9627d18a0ed6
erikmarquez <erikmar3@gmail.com>
2020-10-20 01:06:55 -0500

    added dependacies and first funtion

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+var inquirer = require('inquirer')
+var fs = require("fs")
+const util = require('util')
+const Choices = require("inquirer/lib/objects/choices")
 
+
+
+function prompt(){
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"Title",
+            message:"What is the title of your project?"
+        },
+        {
+            type:"input",
+            name:"Description",
+            message:"What is the title of your project?"
+        },
+        {
+            type:"input",
+            name:"contribution",
+            message:"What are your contribution guidelines?"
+        },
+        {
+            type:"input",
+            name:"test",
+            message:"What are the test instructions your project?"
+        },
+    
+    ])
+}
