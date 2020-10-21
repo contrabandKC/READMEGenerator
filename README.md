52036c30a85129b969eda939fe10d5af21873909
erikmarquez <erikmar3@gmail.com>
2020-10-21 12:04:07 -0500

    First working app

---
+++ README.md
@@ -0,0 +1,41 @@
+
+    # s
+
+    ## Description
+
+    Cars
+
+    ## Table of contents
+
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Licence](#Licence)
+    - [Contributors](#Contributors)
+    - [Test](#Test)
+    - [Repository Link](#Repository)
+    - [GitHub Info](#GitHub) 
+
+    ## Installation
+
+    None
+
+    ## Usage
+
+    Work
+
+    ## Licence
+
+    Apache
+
+    ## Contributors
+
+    undefined
+
+    ## Test
+
+    ## Repository Link
+
+    ## GitHub Info
+
+    
--- index.js
+++ index.js
@@ -3,30 +3,117 @@ var fs = require("fs")
 const util = require('util')
 const Choices = require("inquirer/lib/objects/choices")
 
-
+const writefileAsync = util.promisify(fs.writeFile);
 
 function prompt(){
     return inquirer.prompt([
         {
             type:"input",
-            name:"Title",
+            name:"title",
             message:"What is the title of your project?"
         },
         {
             type:"input",
-            name:"Description",
+            name:"description",
             message:"What is the title of your project?"
+        },
+        {
+            type:"input",
+            name:"installation",
+            message:"What are your contribution guidelines?"
+        },
+        {
+            type:"input",
+            name:"usage",
+            message:"What are the test instructions your project?"
         },
         {
             type:"input",
             name:"contribution",
-            message:"What are your contribution guidelines?"
+            message:"What are the test instructions your project?"
         },
         {
             type:"input",
             name:"test",
             message:"What are the test instructions your project?"
         },
+        {
+            type:"list",
+            name:"license",
+            message:"What is the license for your project?",
+            choices:[
+                "MIT",
+                "Apache",
+                "GPL"
+            ]
+        },
     
     ])
 }
+
+function generateReadMe(answers){
+return `
+    # ${answers.title}
+
+    ## Description
+
+    ${answers.description}
+
+    ## Table of contents
+
+    - [Description](#Description)
+    - [Installation](#Installation)
+    - [Usage](#Usage)
+    - [Licence](#Licence)
+    - [Contributors](#Contributors)
+    - [Test](#Test)
+    - [Repository Link](#Repository)
+    - [GitHub Info](#GitHub) 
+
+    ## Installation
+
+    ${answers.installation}
+
+    ## Usage
+
+    ${answers.usage}
+
+    ## Licence
+
+    ${answers.license}
+
+    ## Contributors
+
+    ${answers.contributors}
+
+    ## Test
+
+    ## Repository Link
+
+    ## GitHub Info
+
+    `
+    } 
+
+
+async function init(){
+    try{
+
+       const answers = await prompt()
+
+       console.log(answers)
+
+       const readme = generateReadMe(answers)
+
+       await writefileAsync("README.md", readme)
+
+       console.log("read me is ready")
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
+} 
+
+init()
